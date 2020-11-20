1f8e1ec04b2d3fcccfac97aa83cbff1d29cd7aa3
Justin Albert <jalbert@carthage.edu>
2020-11-20 17:52:13 -0600

    added email contact

--- routes/api/generic.js
+++ routes/api/generic.js
@@ -1,9 +1,31 @@
 const router = require('express').Router();
+var nodemailer = require('nodemailer');
 
 const genericController = require("../../controllers/genericController");
 
 // Matches with "/api/generic"
 router.route("/")
   .get(genericController.genericTest)
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.USER_NAME,
+      pass: process.env.USER_PASSWORD
+    }
+  });
+
+  router.post("/emailme", (req,res) =>{
+
+    var mailOptions = req.body;
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+
 
 module.exports = router;
